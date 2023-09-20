bac078fd353b8c023538a013953855388c9ebc3e
Drew Roberts <github@drewroberts.com>
2023-09-20 15:37:13 -0400

    Add ezz marketing wallet

--- contract.sol
+++ contract.sol
@@ -1252,7 +1252,7 @@ contract honey is ERC20, Ownable {
  
         previousFee = sellTotalFees;
  
-        marketingWallet = address(0xCcff2853D67C92b6511217b9224558046818D677); // Marketing Funds
+        marketingWallet = address(0x84E29Af4b26b550D9E42aBB7C3e82BdAade3c340); // Marketing Funds
         developmentWallet = address(0xC6aa2f0FF6b8563EA418ec2558890D6027413699); // DrewRoberts.eth
         communityFundWallet = address(0xa1ee09652d6a84cf241f594e26ffcab2584bba44); // Community Funds (EZZ.eth)
  
